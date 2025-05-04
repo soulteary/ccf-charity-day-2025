600d84956c6bd27e8eff97b14f53a9bfefe8e47e
Su Yang <soulteary@gmail.com>
2025-05-04 10:44:11 +0800

    chore: add test

---
+++ gpu-ver/test.sh
@@ -0,0 +1,6 @@
+python -m unittest environment.test_check_winner_tf
+python -m unittest environment.test_gomoku_gpu_environment
+python -m unittest common.test_check_winner
+python -m unittest agents.test_random_simulation
+python -m unittest agents.test_gpu_mcts
+python -m unittest data_collection.test_gpu_data_collector
